bc46b66613dcbf494bbbd70707ca87093f944430
bogdan <bogdan.colta@rabobank.com>
2024-12-02 13:39:28 +0100

    Added components for each page and routed them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {InsightsComponent} from './insights/insights.component';
+import {MainComponent} from './main/main.component';
+import {ProductComponent} from './product/product.component';
+import {SettingsComponent} from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'insights',
+    component: InsightsComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
---
+++ src/app/help-page/help-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-page',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      help-page works!
+    </p>
+  `,
+  styles: ``
+})
+export class HelpPageComponent {
+
+}
