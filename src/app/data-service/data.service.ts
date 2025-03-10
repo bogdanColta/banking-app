d2a2a2bef77b1e99c3fd7402e151e072ae805bbb
bogdan <bogdan.colta@rabobank.com>
2025-03-10 15:09:10 +0100

    Added request headers for fetching transactions

--- src/app/data-service/data.service.ts
+++ src/app/data-service/data.service.ts
@@ -6,12 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic Ym9nZGFuQGdtYWlsLmNvbToxMjM0' // Replace YOUR_TOKEN_HERE with the actual token
+  });
   private url = 'http://localhost:8080/';
 
   constructor(private http: HttpClient) { }
 
   getTransactions(): Observable<any> {
-    return this.http.get(this.url + 'transactions');
+    return this.http.get(this.url + 'transactions', { headers: this.headers });
   }
 }
