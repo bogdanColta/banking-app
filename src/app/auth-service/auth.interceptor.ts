ab38060a6c5752ff0766de2cd780e29b7d82a993
bogdan <bogdan.colta@rabobank.com>
2025-03-31 11:25:03 +0200

    Added redirection to login page when authorization is not set

--- src/app/auth-service/auth.interceptor.ts
+++ src/app/auth-service/auth.interceptor.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authToken = this.authService.getToken();
@@ -14,7 +15,9 @@ export class AuthInterceptor implements HttpInterceptor {
         headers: req.headers.set('Authorization', authToken)
       });
       return next.handle(cloned);
+    } else {
+      this.router.navigate(['/login']);
+      return next.handle(req);
     }
-    return next.handle(req);
   }
 }
